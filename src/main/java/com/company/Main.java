d0079b80d3061fecba4e2bd3ff60fb8f1b0b537d
ShamanDev <pivotguy888@gmail.com>
2020-12-18 23:16:08 +0100

    Added creating article date from url

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -6,16 +6,20 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class Main {
+    public static String createDateString(String[] splited_url){
+        return splited_url[3] + "." + splited_url[2] + "." + splited_url[1]; // DD/MM/YYYY format
+    }
 
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://www.nytimes.com/section/technology").get();
-        Elements articles = doc.select("li.css-ye6x8s");
+        Elements articles = doc.select("div.css-1cp3ece");
         for (Element article : articles) {
-            System.out.println("---------------------------------------------");
+            String date = createDateString(article.select("div.css-1l4spti").select("a").attr("href").split("/"));
             System.out.println("Title: " + article.select("h2.css-1j9dxys ").text());
-            System.out.println("Support text: " + article.select("h2.css-1echdzn").text());
+            System.out.println("Support text: " + article.select("p.css-1echdzn").text());
             System.out.println("Author: " + article.select("span.css-1n7hynb").text());
-            System.out.println("Date: " + article.select("div.css-1lc2l26.span").text());
+            System.out.println("Date: " + date);
+            System.out.println("URL: " + article.select("div.css-1l4spti").select("a").attr("href"));;
             System.out.println("---------------------------------------------");
         }
     }
