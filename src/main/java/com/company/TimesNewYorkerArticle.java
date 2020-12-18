48dd35fc3e062dcae7193ec817365f2ae4be6ca4
ShamanDev <pivotguy888@gmail.com>
2020-12-18 22:34:22 +0100

    Added TimesNewYorkerArticle with basic constructor

---
+++ src/main/java/com/company/TimesNewYorkerArticle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.util.Date;
+
+public class TimesNewYorkerArticle {
+    private String title;
+    private String support_text;
+    private String author;
+    private Date date;
+    private String url;
+}
