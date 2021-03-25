ab85d4aa7f8edb1192b3fbfd8b636f3bac75e3b2
Kamil Paczkowski <pivotguy888@gmail.com>
2021-03-25 01:59:49 +0100

    Add files via upload

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -7,6 +7,8 @@ import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 
 public class Main {
     public static String createDateStringFromUrl(String[] splited_url){
@@ -23,42 +25,79 @@ public class Main {
         return false;
     }
 
+    public static void readArticlesFromDB() {
+        long begin_time = System.currentTimeMillis();
+        long end_time = System.currentTimeMillis();
+
+        try {
+            NewYorkScraperSQLConnector connector = new NewYorkScraperSQLConnector();
+            connector.readFromDB();
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
+
+        long exec_time_nanoseconds = (end_time - begin_time);
+        System.out.println("Data parsed in: "+exec_time_nanoseconds+" nanoseconds");
+    }
+
     public static void saveArticlesToDB(ArrayList<TimesNewYorkerArticle> articles) throws SQLException, IllegalAccessException, InstantiationException, ClassNotFoundException {
+        long begin_time = System.currentTimeMillis();
         try {
             NewYorkScraperSQLConnector connector = new NewYorkScraperSQLConnector();
             connector.saveToDB(articles);
         } catch (SQLException err) {
             throw err;
         }
+        long end_time = System.currentTimeMillis();
+        long exec_time_nanoseconds = (end_time - begin_time);
+        System.out.println("Data added in: "+exec_time_nanoseconds+" nanoseconds");
     }
 
     public static void main(String[] args) throws IOException, ParseException, SQLException, IllegalAccessException, ClassNotFoundException, InstantiationException {
-        ArrayList<TimesNewYorkerArticle> articles = new ArrayList<TimesNewYorkerArticle>();
-        Document doc = Jsoup.connect("https://www.nytimes.com/section/technology").get();
-        Elements scrapped_articles = doc.select("div.css-1cp3ece");
-        for (Element article : scrapped_articles) {
-            try {
-                String title = article.select("h2.css-1j9dxys ").text();
-                String support_text = article.select("p.css-1echdzn").text();
-                String author = article.select("span.css-1n7hynb").text();
-                String date = createDateStringFromUrl(article.select("div.css-1l4spti").select("a").attr("href").split("/"));
-                String url = article.select("div.css-1l4spti").select("a").attr("href");
-                TimesNewYorkerArticle new_article = new TimesNewYorkerArticle(
-                        title = title,
-                        support_text = support_text,
-                        author = author,
-                        date = date,
-                        url = url
-                );
-                if (isArticleInArticles(articles, new_article) == false) {
-                    articles.add(new_article);
-                }
-            } catch (ParseException e) {
-                continue;
-            }
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("1. Odczytaj dane z bazy");
+        System.out.println("2. Scrapuj dane do bazy");
+        System.out.print("Twój wybór:");
+        String choice = scanner.nextLine();
+
+        if (choice.equals("1")) {
+            readArticlesFromDB();
+            System.out.println("Poprawnie odczytano artykuły z bazy danych!");
         }
 
-        saveArticlesToDB(articles);
-        System.out.println("Articles scraped properly!");
+        else if (choice.equals("2")) {
+            ArrayList<TimesNewYorkerArticle> articles = new ArrayList<TimesNewYorkerArticle>();
+            Document doc = Jsoup.connect("https://www.nytimes.com/section/technology").get();
+            Elements scrapped_articles = doc.select("div.css-1cp3ece");
+            for (Element article : scrapped_articles) {
+                try {
+                    String title = article.select("h2.css-1j9dxys ").text();
+                    String support_text = article.select("p.css-1echdzn").text();
+                    String author = article.select("span.css-1n7hynb").text();
+                    String date = createDateStringFromUrl(article.select("div.css-1l4spti").select("a").attr("href").split("/"));
+                    String url = article.select("div.css-1l4spti").select("a").attr("href");
+                    TimesNewYorkerArticle new_article = new TimesNewYorkerArticle(
+                            title = title,
+                            support_text = support_text,
+                            author = author,
+                            date = date,
+                            url = url
+                    );
+                    if (!isArticleInArticles(articles, new_article)) {
+                        articles.add(new_article);
+                    }
+                } catch (ParseException e) {
+                    continue;
+                }
+            }
+
+            saveArticlesToDB(articles);
+            System.out.println("Poprawnie dodano pobrane artykuły do bazy danych!");
+        }
+
+        else {
+            System.out.println(choice);
+            System.out.println("Podano niepoprawny wybór");
+        }
     }
 }
--- src/main/java/com/company/NewYorkScraperSQLConnector.java
+++ src/main/java/com/company/NewYorkScraperSQLConnector.java
@@ -7,24 +7,37 @@ public class NewYorkScraperSQLConnector {
     private Connection connection;
 
     public NewYorkScraperSQLConnector() throws SQLException{
-        Connection conn = DriverManager.getConnection(
+        connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/newyorknews",
                 "root",
                 ""
         );
-        connection = conn;
     }
 
     public void saveToDB(ArrayList<TimesNewYorkerArticle> articles) throws SQLException {
         Statement stmt = connection.createStatement();
         for (TimesNewYorkerArticle article : articles) {
-            String sql = String.format("INSERT INTO news(title, support_text, author, date, url) VALUES (%s, %s, %s, %s, %s)",
+            String sql = String.format("INSERT INTO news(title, support_text, author, date, url) VALUES ('%s', '%s', '%s', '%s', '%s')",
                     article.getTitle(),
                     article.getSupportText(),
                     article.getAuthor(),
                     article.getDate(),
                     article.getUrl());
             stmt.executeUpdate(sql);
+        }
+    }
+
+    public void readFromDB() throws SQLException {
+        Statement stmt = connection.createStatement();
+        String sql = "SELECT * FROM news";
+        ResultSet rs = stmt.executeQuery(sql);
+        while (rs.next()) {
+            System.out.println(rs.getString("title"));
+            System.out.println(rs.getString("support_text"));
+            System.out.println(rs.getString("author"));
+            System.out.println(rs.getString("date"));
+            System.out.println(rs.getString("url"));
+            System.out.println("---------------------------------------------");
         }
     }
 }
